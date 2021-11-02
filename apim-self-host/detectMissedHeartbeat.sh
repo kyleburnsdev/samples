cba3405a6f0873b1697b1a40da779cc40a3e4151
Kyle Burns <31851557+kyleburnsdev@users.noreply.github.com>
2021-11-01 23:14:47 -0400

    Update detectMissedHeartbeat.sh

--- apim-self-host/detectMissedHeartbeat.sh
+++ apim-self-host/detectMissedHeartbeat.sh
@@ -10,11 +10,19 @@
 # ESTABLISHED FOR USE OF THE az CLI COMMAND. IT DEPENDS
 # ON THE FOLLOWING PACKAGES:
 # - azure-cli (https://docs.microsoft.com/en-us/cli/azure/)
+# - jq (https://stedolan.github.io/jq/)
 #
 RESOURCE_GROUP=**YOUR_RESOURCE_GROUP_NAME**
 APIM_NAME=**YOUR_APIM_INSTANCE_NAME**
 MIN_HEARTBEAT=$(date +"%Y-%m-%dT%H:%m:00Z" -d "-30 minutes") #adjust to your tolerance
-az rest --method GET --uri /subscriptions/{subscriptionId}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.ApiManagement/service/${APIM_NAME}/gateways?api-version=2021-01-01-preview --query "value[?properties.heartbeat[-1].timestamp < '${MIN_HEARTBEAT}'].{name: name, lastHeartbeat: properties.heartbeat[-1].timestamp}"
+RESULT=$(az rest --method GET --uri /subscriptions/{subscriptionId}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.ApiManagement/service/${APIM_NAME}/gateways?api-version=2021-01-01-preview --query "value[?properties.heartbeat[-1].timestamp > '${MIN_HEARTBEAT}'].{name: name, lastHeartbeat: properties.heartbeat[-1].timestamp}")
+MISSED_COUNT=$(echo $RESULT | jq length)
 
-# THIS IS WHERE YOU WOULD DO SOMETHING USEFUL WITH THE RESULT SUCH AS RAISING ALERTS OR CREATING TROUBLE TICKETS
-# THIS IS NOT DEMONSTRATED IN THIS BECAUSE IT WILL VARY GREATLY BY ENVIRONMENT
+if [ $(($MISSED_COUNT)) > 0 ]
+then
+    echo "$MISSED_COUNT is greater than 0"
+    # THIS IS WHERE YOU WOULD DO SOMETHING USEFUL WITH THE RESULT SUCH AS RAISING ALERTS OR CREATING TROUBLE TICKETS
+    # NOT DEMONSTRATED IN THIS SCRIPT BECAUSE IT WILL VARY GREATLY BY ENVIRONMENT
+else
+    echo "no heartbeats missed"
+fi
